a02c904d44483386c7724b44ffcc3d4ae0a6f243
Abyan Falah <abyanf410@gmail.com>
2023-05-08 05:52:56 +0700

    add event propagation to prevent calling double event

--- src/components/todoapp/TaskGroupSelect.vue
+++ src/components/todoapp/TaskGroupSelect.vue
@@ -6,82 +6,81 @@ import getBrightorDarkTextColor from '@/services/getBrightorDarkTextColor';
 const store = useTodoappStore();
 
 function hey() {
-  alert("button merah ditekan!");
+	alert("button merah ditekan!");
 }
 
 </script>
 
 <template>
-  <v-sheet style="min-height: 100vh;"
-           color="blue"
-           class="d-flex flex-column justify-center align-center">
+	<v-sheet style="min-height: 100vh;"
+					 color="blue"
+					 class="d-flex flex-column justify-center align-center">
 
-    <!-- hello to user -->
-    <v-sheet color="transparent"
-             class="d-flex flex-column justify-center align-center mb-10">
-      <p class="text-h4">
-        Hi
-      </p>
-      <input type="text"
-             class="text-h3"
-             style="outline: none; text-align: center;"
-             v-model="store.username"
-             placeholder="[Wholesome anon]">
-      <small v-if="!store.username"
-             class="text-disabled">Your name here ^</small>
-      <p class="my-3">Let's get them tasks done!</p>
-    </v-sheet>
+		<!-- hello to user -->
+		<v-sheet color="transparent"
+						 class="d-flex flex-column justify-center align-center mb-10">
+			<p class="text-h4">
+				Hi
+			</p>
+			<input type="text"
+						 class="text-h3"
+						 style="outline: none; text-align: center;"
+						 v-model="store.username"
+						 placeholder="[Wholesome anon]">
+			<small v-if="!store.username"
+						 class="text-disabled">Your name here ^</small>
+			<p class="my-3">Let's get them tasks done!</p>
+		</v-sheet>
 
-    <!-- task group list button -->
-    <v-sheet class="d-flex justify-center align-center flex-wrap align-content-space-between"
-             max-width="80%"
-             color="transparent">
+		<!-- task group list button -->
+		<v-sheet class="d-flex justify-center align-center flex-wrap align-content-space-between"
+						 max-width="80%"
+						 color="transparent">
 
 
-      <!-- taskgroup button -->
-      <v-card v-for="(taskGroup, index) in store.taskGroupList"
-              role="button"
-              class="mx-3 mb-6 d-flex justify-center align-center"
-              width="150px"
-              elevation="5"
-              @click="store.toggleSelectedTaskGroup(taskGroup)"
-              :class="`text-${getBrightorDarkTextColor(taskGroup.color)}`"
-              :color="taskGroup.color"
-              height="150px">
+			<!-- taskgroup button -->
+			<v-card v-for="(taskGroup, index) in store.taskGroupList"
+							role="button"
+							class="mx-3 mb-6 d-flex justify-center align-center"
+							width="150px"
+							elevation="5"
+							@click="store.toggleSelectedTaskGroup(taskGroup)"
+							:class="`text-${getBrightorDarkTextColor(taskGroup.color)}`"
+							:color="taskGroup.color"
+							height="150px">
 
-        <!-- taskgroup option container -->
-        <v-sheet class="align-self-start d-flex justify-end"
-                 position="absolute"
-                 location="top"
-                 width="100%"
-                 color="transparent"
-                 style="z-index: 9;">
+				<!-- taskgroup option container -->
+				<v-sheet class="align-self-start d-flex justify-end"
+								 position="absolute"
+								 location="top"
+								 width="100%"
+								 color="transparent">
 
-          <v-btn icon="mdi-dots-vertical"
-                 variant="elevated"
-                 color="red"
-                 @click="hey()"></v-btn>
+					<v-btn icon="mdi-dots-vertical"
+								 variant="elevated"
+								 color="red"
+								 @click.stop="hey()"></v-btn>
 
-        </v-sheet>
-        <p>
-          {{ taskGroup.name }}
-        </p>
-      </v-card>
+				</v-sheet>
+				<p>
+					{{ taskGroup.name }}
+				</p>
+			</v-card>
 
-      <!-- new taskgroup button -->
-      <v-card role="button"
-              class="mx-3 mb-6  d-flex justify-center flex-column align-center elevation-0 border"
-              width="150px"
-              color="white"
-              @click="store.newTaskGroup"
-              height="150px">
-        <v-icon icon="mdi-plus"
-                class="text-h4"></v-icon>
-        <p>New Taskgroup</p>
-      </v-card>
+			<!-- new taskgroup button -->
+			<v-card role="button"
+							class="mx-3 mb-6  d-flex justify-center flex-column align-center elevation-0 border"
+							width="150px"
+							color="white"
+							@click="store.newTaskGroup"
+							height="150px">
+				<v-icon icon="mdi-plus"
+								class="text-h4"></v-icon>
+				<p>New Taskgroup</p>
+			</v-card>
 
-    </v-sheet>
-  </v-sheet>
+		</v-sheet>
+	</v-sheet>
 </template>
 
 
